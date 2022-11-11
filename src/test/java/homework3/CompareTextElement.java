33ec6661e74ed0c824684fa193cf9c09e77de716
MANKRA <kristina.manevica@if.lv>
2022-11-11 10:10:03 +0200

    Lection about cikl

--- src/test/java/homework3/CompareTextElement.java
+++ src/test/java/homework3/CompareTextElement.java
@@ -1,5 +1,6 @@
 package homework3;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,32 +14,42 @@ import java.util.List;
 
 public class CompareTextElement {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
-    private final By LATVIA_COUNTRY_HREF = By.xpath(".//a[@href='https://www.discovercars.com/latvia']");
+    private final By TOP_LOCATION_LINK = By.xpath(".//div[@class='tl-box']/a");
+
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @Test
     public void searchClickElement() {
+        String country = "Latvia";
+
         System.setProperty("webdriver.chrome.driver", "C:\\KM\\aqa\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-
         driver.get("http://discovercars.com/");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
 
         driver.findElement(ACCEPT_COOKIES_BTN).click();
 
-        List<WebElement> firstElement = driver.findElements(By.xpath(".//h1[contains(@class, 'home-h1 display-small')]"));
+        clickCountry(country);
+    }
 
-        driver.findElement(LATVIA_COUNTRY_HREF).click();
+    private void clickCountry(String country) {
+        List<WebElement> links = driver.findElements(TOP_LOCATION_LINK);
 
-        List<WebElement> secondElement = driver.findElements(By.xpath(".//h1[contains(@class, 'home-h1 display-small')]"));
+        boolean isCountryFound = false;
 
-        if (firstElement.equals(secondElement)) {
-            System.out.println("Something is wrong");
-        } else {
-            System.out.println("It is correct, two different texts in element");
+        for (WebElement link : links) {
+            if (link.getText().equals(country)) {
+                isCountryFound = true;
+                wait.until(ExpectedConditions.elementToBeClickable(link));
+                link.click();
+                break;
+            }
         }
+        Assertions.assertTrue(isCountryFound, "Country not found!");
     }
 }
+
