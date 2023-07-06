08a6cc8bf69dcd5c63114d4e84348381a5b6db88
MANKRA <kristina.manevica@if.lv>
2023-07-06 18:11:14 +0300

    Cucumber, Gherkin

---
+++ src/test/java/stepdefs/FirstStepDefs.java
@@ -0,0 +1,10 @@
+package stepdefs;
+
+import io.cucumber.java.en.When;
+
+public class FirstStepDefs {
+    @When("user prints welcome text")
+    public void print_welcome_text(){                    //vsegda public, zapuskajetsja junit izvne, prinjato underscore
+        System.out.println("Hello World!");
+    }
+}
